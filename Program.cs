1ac03d01ae435804239829151cd33cbd044829e8
Alex <ianaalexandru03@gmail.com>
2025-02-06 18:46:54 +0100

    hotfix program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,43 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString(""); //Poner la nuestra que sea necesaria.
+
+// Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services 
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors(configurePolicy: policy =>
+{
+    // policy.WithOrigins("*","https://localhost","http://localhost");
+    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+});
+
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
